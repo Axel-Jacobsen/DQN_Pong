3cf9b5710cbe8d464d9e374350b6140b3fccacd3
Axel-Jacobsen <axelnj44@gmail.com>
2019-12-03 13:42:58 +0100

    added policies from previous training

--- train.py
+++ train.py
@@ -225,7 +225,7 @@ if __name__ == '__main__':
     device = torch.device(
             "cuda" if torch.cuda.is_available() else "cpu"
             )
-    
+
     print(f'Using Device {device}')
 
     target = DQN(device=device).to(device)
